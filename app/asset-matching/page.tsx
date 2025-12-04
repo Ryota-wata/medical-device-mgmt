d4db61d974fb480ff004718c1de7d6b9495bb9b6
Ryota Watanabe <ryota.watanabe@boxyz.com>
2025-12-04 21:50:10 +0900

    資産マスタ別ウィンドウでSHIP資産マスタのスタンドアロン版を開くように変更

--- app/asset-matching/page.tsx
+++ app/asset-matching/page.tsx
@@ -395,7 +395,7 @@ export default function AssetMatchingPage() {
     const top = (window.screen.height - height) / 2;
 
     window.open(
-      '/asset-master',
+      'http://localhost:8080/ship-asset-master-standalone.html',
       'AssetMasterWindow',
       `width=${width},height=${height},left=${left},top=${top},resizable=yes,scrollbars=yes`
     );
